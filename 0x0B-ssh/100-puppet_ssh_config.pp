493fb76a30cf9bb75a7f7809f658d1eb19812e75
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-01-11 16:36:36 +0300

    ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# changes to the configuration file, disable authentication
+
+file_line { 'Declare identity file':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
+  replace => true,
+}
+
+file_line { 'Turn off passwd auth':
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
+}
