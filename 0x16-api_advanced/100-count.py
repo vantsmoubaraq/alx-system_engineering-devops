204bf6304d7bbe512429d1f1beb1fd263d58b81f
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-02-28 20:50:08 +0300

    api_advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,54 +1,60 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit."""
-import requests
+"""
+This script defines a function to print a sorted count of given keywords
+present inside a given subreddit using recursion
+"""
 
 
-def count_words(subreddit, word_list, instances={}, after="", count=0):
-    """Prints counts of given words found in hot posts of a given subreddit.
-    Args:
-        subreddit (str): The subreddit to search.
-        word_list (list): The list of words to search for in post titles.
-        instances (obj): Key/value pairs of words/counts.
-        after (str): The parameter for the next page of the API results.
-        count (int): The parameter of results matched thus far.
+def count_words(subreddit, word_list, after='start', words_count=None):
     """
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "after": after,
-        "count": count,
-        "limit": 100
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    try:
-        results = response.json()
-        if response.status_code == 404:
-            raise Exception
-    except Exception:
-        print("")
-        return
+    Prints sorted count of given words present in subreddit's hot titles
+    using the reddit API
+    Example:
+    >>> words_list = ["react", "python", "java"]
+    >>> count("programming", words_list)
+    java: 27
+    python: 17
+    react: 17
+    Args:
+    subreddit (str): The subreddit to be searched for
+    word_list (list): List of keywords to count
+    after (str): Index to keep tack of remaining pages `NOT TO BE CHANGED`
+    words_count (dict): Keep track of word's count `NOT TO BE CHANGED`
+    Returns
+    int: 1 valid subreddit or 0 for invalid subreddit
+    NOTE:
+    The function prints the count result internally
+    """
+    import requests
 
-    results = results.get("data")
-    after = results.get("after")
-    count += results.get("dist")
-    for c in results.get("children"):
-        title = c.get("data").get("title").lower().split()
-        for word in word_list:
-            if word.lower() in title:
-                times = len([t for t in title if t == word.lower()])
-                if instances.get(word) is None:
-                    instances[word] = times
-                else:
-                    instances[word] += times
+    if not word_list:
+        return 0
 
-    if after is None:
-        if len(instances) == 0:
-            print("")
-            return
-        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
-        [print("{}: {}".format(k, v)) for k, v in instances]
+    if words_count is None:
+        words_count = {word.lower(): 0 for word in word_list}
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"user-agent": "Fake-Agent"}
+    if after != 'start':
+        url += "?after={}".format(after)
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+
+    for post in response.json().get('data').get('children'):
+        title = post.get('data').get('title')
+        for word in title.split():
+            if word.lower() in words_count.keys():
+                words_count[word.lower()] += 1
+
+    after = response.json().get('data').get('after')
+    if after:
+        return count_words(subreddit, word_list, after, words_count)
     else:
-        count_words(subreddit, word_list, instances, after, count)
+        words_count = dict(sorted(words_count.items(),
+                                  key=lambda x: x[1], reverse=True))
+        for word, count in words_count.items():
+            if count != 0:
+                print(str(word) + ": " + str(count))
+        return 1
